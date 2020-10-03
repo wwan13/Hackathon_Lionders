738226cf260d59e2e7899ef4d6ba02329d3b3a7e
김율희 <dbfgml741@likelion.org>
2020-10-03 22:16:21 +0900

    [YulHee] order sheet 생성,수정,삭제,조회

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('order/',views.order,name='order'),
+    path('order/orderlist/',views.order_list,name='order-list'),
+    path('order/<int:id>/delete/', views.order_delete,name='order-delete'),
+    path('order/update/',views.order_update,name='order-update'),
 ]
