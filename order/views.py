39c2790c1fd48ec6fc5fed73c7db40f123c84141
KimTaewan <wwan13@naver.com>
2020-10-10 23:23:24 +0900

    [taewan] order-item many-to-many

--- order/views.py
+++ order/views.py
@@ -30,13 +30,28 @@ def order_list(request):
 #     return render(request, 'order.html', {'form': form})
 
 def order(request):
-    items = Item.objects.all()
-    current_user = get_object_or_404(usermodel.Consumer_Users,pk = request.user.id)
-    if current_user.have_order_sheet == False:
-        new_order = Order(normal_user_info=current_user,total_price=0)
-        new_order.save()
-    # return render(request,'order.html',{'items':items,'new_order':new_order})
-    return render(request,'order.html',{'items':items})
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('require_login')
+        else:
+            items = Item.objects.all()
+            current_user = get_object_or_404(usermodel.Consumer_Users,pk = request.user.id)
+            if current_user.have_order_sheet == False:
+                new_order = Order(normal_user_info=current_user,total_price=0)
+                new_order.save()
+                current_user.have_order_sheet = True
+                current_user.save()
+                ordered_items = new_order.items.all()
+                ex_items = list(set(items) - set(ordered_items))
+                return render(request,'order.html',{'items':items,'order':new_order,'ordered_items':ordered_items,'ex_items':ex_items})
+            else:
+                my_order = current_user.order_set.all().order_by('-ordered_time')
+                ordered_items = my_order[0].items.all()
+                ex_items = list(set(items) - set(ordered_items))
+                return render(request,'order.html',{'items':items,'order':my_order[0],'ordered_items':ordered_items,'ex_items':ex_items})
+    else:
+        return redirect('require_login')
+
 
 
 def add_item(request,order_id,item_id):
