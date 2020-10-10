12fb760f00e7506823c43dede9af7bc48858d36a
KimTaewan <wwan13@naver.com>
2020-10-10 21:39:57 +0900

    [taewan] update order page

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"index.html")
+
+def require_login(request):
+    return render(request,"require_login.html")
