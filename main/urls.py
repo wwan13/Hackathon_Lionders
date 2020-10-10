f2cfc7f1d88c880ecc979704a72acb333301c562
김율희 <dbfgml741@likelion.org>
2020-10-11 00:02:34 +0900

    [YulHee] detail 페이지 생성

--- main/urls.py
+++ main/urls.py
@@ -18,4 +18,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('detail',views.detail,name='detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"index.html")
+
+def detail(request):
+    return render(request,"detail.html")
