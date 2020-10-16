219032423b9909c9c0f44b531181b8387a790e3a
KimTaewan <wwan13@naver.com>
2020-10-16 14:15:31 +0900

    [taewan] auto create item objects

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from order import models as ordermodel
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,20 @@ def detail(request):
   
 def require_login(request):
     return render(request,"require_login.html")
+
+def create_objs(request):
+    items = ordermodel.Item.objects.all()
+    return render(request,"create_objs.html",{'items':items})
+
+def create_item_objects(requset):
+    file = open('main/textfiles/items.txt', mode='rt', encoding='utf-8')
+    lists = file.readlines()
+    for list in lists:
+        tmp = list.split(' ')
+        item = ordermodel.Item(
+            store = tmp[0],
+            item_name = tmp[1],
+            price = int(tmp[2]),
+        )
+        item.save()
+    return redirect("create_objs")
