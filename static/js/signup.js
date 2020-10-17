97f6932f6f11fcf47fd45bcee8c250c35a4c3a1d
KimTaewan <wwan13@naver.com>
2020-10-17 20:44:42 +0900

    [taewan] 도로명주소 api 테스트

--- static/js/signup.js
+++ static/js/signup.js
@@ -6,3 +6,12 @@ $(".signupbox__textbox input").on("blur", function () {
     if ($(this).val() == "")
         $(this).removeClass("focus");
 });
+
+$(".text__address_num input").on("focus", function () {
+    $(this).addClass("focus");
+});
+
+$(".text__address_num input").on("blur", function () {
+    if ($(this).val() == "")
+        $(this).removeClass("focus");
+});
