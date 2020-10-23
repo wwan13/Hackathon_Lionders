a9f63e4167baa3cdad94dfed5fd3ab0c9e0f022f
ningpop <craft1933@naver.com>
2020-10-24 02:37:59 +0900

    [ningpop] add user's period in order list page

--- order/urls.py
+++ order/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('order/',views.order,name='order'),
     path('order_final/<int:order_id>',views.make_order_final,name='order_final'),
     path('order/orderlist/',views.order_list,name='order-list'),
+    path('order/<int:order_id>/',views.order_detail,name='order-detail'),
     path('order/<int:id>/delete/', views.order_delete,name='order-delete'),
     path('order/update/',views.order_update,name='order-update'),
     path('item/',views.item_create,name='item'),
