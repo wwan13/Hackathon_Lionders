ad4a28300820c78a1ec37538141eb1d51f80c650
KimTaewan <wwan13@naver.com>
2020-10-24 01:25:43 +0900

    [taewn] fix view.py

--- delivery/views.py
+++ delivery/views.py
@@ -7,23 +7,6 @@ from order import models as ordermodel
 # Create your views here.
 
 
-# def delivery_list(request):
-#     if not request.user.is_authenticated:
-#         return HttpResponseRedirect('/signin/')
-#     if request.user.usertype == 'lionders':
-#         deliveries = Delivery.objects.filter(order_sheet__destination = request.user.address, lionders_info_id=request.user.pk)
-#         context = {
-#             'deliveries': deliveries,
-#         }
-#         return render(request, 'delivery_list.html', context)
-#     else :
-#         deliveries = Delivery.objects.filter(lionders_info_id=request.user.pk)
-#         context = {
-#             'deliveries': deliveries,
-#         }
-#         return render(request, 'delivery_list.html', context)
-
-
 def delivery_list(request):
     orders = ordermodel.Order.objects.filter(is_complete=False,can_delivery=True)
     return render(request,'delivery_list.html',{'orders':orders})
@@ -56,18 +39,6 @@ def make_delivery(request, order_id):
     return render(request,"delivery.html",{'delivery':delivery})
 
 
-# def delivery(request):
-#     if request.method == 'POST':
-#         form = DeliveryForm(request.POST)
-#         request.POST._mutable = True
-#         request.POST['lionders_info'] = request.user
-#         if form.is_valid():
-#             new_item = form.save()
-#         return HttpResponseRedirect('../delivery/deliverylist')
-#     form = DeliveryForm()
-#     return render(request, 'delivery.html', {'form': form})
-
-
 def delivery_update(request, id):
     item = get_object_or_404(Delivery, pk=id)
     if request.method == 'POST':
@@ -83,7 +54,3 @@ def delivery_delete(request, id):
         item.delete()
         return redirect('delivery-list')  # 리스트 화면으로 이동합니다.
     return render(request, 'delivery_delete.html', {'item': item})
-
-
-
-
